3af69b7b3ef53df465f1c44c5daedfcdf18dcc5c
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-07-06 08:17:57 +0000

    Initial commit

---
+++ src/validationSchema/employees/index.ts
@@ -0,0 +1,7 @@
+import * as yup from 'yup';
+
+export const employeeValidationSchema = yup.object().shape({
+  time_tracking: yup.number().integer(),
+  performance_evaluation: yup.number().integer(),
+  user_id: yup.string().nullable(),
+});
